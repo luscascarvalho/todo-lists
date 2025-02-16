400085b030513b8b476741679a690c3201288136
Lucas Carvalho <lucascarvalho.mug@gmail.com>
2025-02-16 12:11:00 -0300

    feat: finalizar página de login

--- my-app/src/pages/login/index.tsx
+++ my-app/src/pages/login/index.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { style } from "./styles";
 import Logo from "../../assets/logo.png";
@@ -8,9 +16,32 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { themas } from "../../global/themes";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  async function getLogin() {
+    try {
+      setLoading(true);
+
+      if (!email || !password) {
+        Alert.alert("Atenção", "Informe os campos obrigatórios!");
+        return setLoading(false); 
+      }
+
+      setTimeout(() => {
+        if (email == "usuario@gmail.com" && password == "123456") {
+          Alert.alert("Logado com sucesso!");
+        } else {
+          Alert.alert("Usuário não encontrado!");
+        }
+
+        setLoading(false);
+      }, 3000);
+    } catch (error) {
+      console.log("error");
+    }
+  }
 
   return (
     <View style={style.container}>
@@ -22,12 +53,20 @@ export default function Login() {
       <View style={style.boxMid}>
         <Text style={style.titleInput}>E-mail</Text>
         <View style={style.boxInput}>
-          <TextInput style={style.input} value={email} onChangeText={(e) => setEmail(e)}/>
+          <TextInput
+            style={style.input}
+            value={email}
+            onChangeText={(e) => setEmail(e)}
+          />
           <MaterialIcons name="email" size={20} color={themas.color.gray} />
         </View>
         <Text style={style.titleInput}>Senha</Text>
         <View style={style.boxInput}>
-          <TextInput style={style.input} value={password} onChangeText={(e) => setPassword(e)}/>
+          <TextInput
+            style={style.input}
+            value={password}
+            onChangeText={(e) => setPassword(e)}
+          />
           <MaterialIcons
             name="remove-red-eye"
             size={20}
@@ -37,11 +76,18 @@ export default function Login() {
       </View>
 
       <View style={style.boxBottom}>
-        <TouchableOpacity style={style.button}>
-          <Text style={style.textButton}>Entrar</Text>
+        <TouchableOpacity style={style.button} onPress={() => getLogin()}>
+          {loading ? (
+            <ActivityIndicator color={"#FFFFFF"} size={"small"} />
+          ) : (
+            <Text style={style.textButton}>Entrar</Text>
+          )}
         </TouchableOpacity>
       </View>
-      <Text style={style.textBottom}>Não tem conta? <Text style={{ color: themas.color.primary }}>Crie agora!</Text></Text>
+      <Text style={style.textBottom}>
+        Não tem conta?{" "}
+        <Text style={{ color: themas.color.primary }}>Crie agora!</Text>
+      </Text>
     </View>
   );
 }
