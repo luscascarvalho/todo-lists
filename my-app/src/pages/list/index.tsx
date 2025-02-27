7b7861424a189a887742c0db2c8c7ac608c91953
Lucas Carvalho <lucascarvalho.mug@gmail.com>
2025-02-27 11:56:46 -0300

    feat: iniciar página List

--- my-app/src/pages/list/index.tsx
+++ my-app/src/pages/list/index.tsx
@@ -1,11 +1,52 @@
 import React from "react";
 
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { style } from "./style";
+import { Input } from "../../components/input/intex";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const data: any = [
+  {
+    item: 0,
+    title: "Realizar a tarefa de casa",
+    description: "página 10 a 20",
+    flag: "urgente",
+  },
+
+  {
+    item: 1,
+    title: "Passear com a Malu",
+    description: "16h30",
+    flag: "urgente",
+  },
+
+  {
+    item: 2,
+    title: "Ir para a academia",
+    description: "10h",
+    flag: "urgente",
+  },
+];
 
 export default function List() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Olá mundo das listas doida</Text>    
+    <View style={style.container}>
+      <View style={style.header}>
+        <Text style={style.greeting}>
+          Bom dia,{" "}
+          <Text style={{ fontWeight: "bold", fontStyle: "italic" }}>
+            seu doidinho
+          </Text>
+        </Text>
+
+        <View style={style.boxInput}>
+          <Input IconLeft={MaterialIcons} IconLeftName="search" />
+        </View>
+      </View>
+
+      <View style={style.boxList}>
+        <FlatList />
+      </View>
     </View>
   );
 }
--- my-app/src/pages/list/style.ts
+++ my-app/src/pages/list/style.ts
@@ -2,5 +2,33 @@ import { StyleSheet, Dimensions } from "react-native";
 import { themas } from "../../global/themes";
 
 export const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    }, 
+
+    header: {
+        width: '100%',
+        height: Dimensions.get('window').height/6,
+        backgroundColor: themas.color.primary,
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+    },
+
+    greeting: {
+        fontSize: 20,
+        color: '#FFFFFF',
+        marginTop: 20
+    }, 
+
+    boxInput: {
+        width: '80%'
+    },
+
+    boxList: {
+        flex: 1,
+        width: '100%',
+    }
+
     
 })
