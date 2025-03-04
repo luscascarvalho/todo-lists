d00345452b9a835e5661c531a59f641972f1be0a
Lucas Carvalho <lucascarvalho.mug@gmail.com>
2025-03-04 11:13:55 -0300

    feat: criar modal de criação de tasks

--- my-app/src/context/authContext_list.tsx
+++ my-app/src/context/authContext_list.tsx
@@ -1,6 +1,15 @@
 import React, { createContext, useContext, useRef } from "react";
-import { Alert, Dimensions, Text } from "react-native";
+import {
+  Alert,
+  Dimensions,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { Modalize } from "react-native-modalize";
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
+import { Input } from "../components/input/intex";
 
 export const AuthContextList: any = createContext({});
 
@@ -8,19 +17,49 @@ export const AuthProviderList = (props: any): any => {
   const modalizeRef = useRef<Modalize>(null);
 
   const onOpen = () => {
-    Alert.alert("Atenção", "Modal aberto");
+    modalizeRef.current?.open();
   };
 
   const container = () => {
-    return <Text></Text>
-  }
+    return (
+      <View style={style.container}>
+        <View style={style.header}>
+          <TouchableOpacity>
+            <MaterialIcons name="close" size={30} />
+          </TouchableOpacity>
+
+          <Text style={style.title}>Criar tarefa</Text>
+
+          <TouchableOpacity>
+            <AntDesign name="check" size={30} />
+          </TouchableOpacity>
+        </View>
+        <View style={style.content}>
+          <Input title="Titulo:" />
+          <Input title="Descrição:"/>
+
+          <View style={{ width: "40%" }}>
+            <Input title="Tempo limite:"/>
+          </View>
+
+          <View style={style.containerFlag}> 
+              <Text>Flags: </Text>
+              <View style={{}}>
+
+              </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <AuthContextList.Provider value={{ onOpen }}>
       {props.children}
       <Modalize
         ref={modalizeRef}
-        modalHeight={Dimensions.get("window").height / 1.3}
+        childrenStyle={{ height: Dimensions.get("window").height / 1.3 }}
+        adjustToContentHeight={true}
       >
         {container()}
       </Modalize>
@@ -29,3 +68,34 @@ export const AuthProviderList = (props: any): any => {
 };
 
 export const useAuth = () => useContext(AuthContextList);
+
+export const style = StyleSheet.create({
+  container: {
+    width: '100%'
+  },
+
+  header: {
+    width: '100%',
+    height: 40,
+    paddingHorizontal: 40,
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 20, 
+    fontWeight: 'bold',
+
+  },
+
+  content: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+
+  containerFlag: {
+    width: '100%'
+  }
+});
