e14bdc796ed782969d59b6f13da4d6184c92ca7a
Lucas Carvalho <lucascarvalho.mug@gmail.com>
2025-03-26 08:54:46 -0300

    fix: tipar isoDate como string

--- my-app/src/global/function.tsx
+++ my-app/src/global/function.tsx
@@ -1,16 +1,14 @@
-const formatDateToBR = (isoDate) => {
-    const date = new Date(isoDate)
+const formatDateToBR = (isoDate: string): string => {
+  const date = new Date(isoDate);
 
-    const day = String(date.getDate()).padStart(2, '0')
-    const month = String(date.getMonth() + 1).padStart(2, '0')
-    const year = date.getFullYear()
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
 
-    const hours = String(date.getHours()).padStart(2, '0')
-    const minute = String(date.getMinutes()).padStart(2, '0')
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
 
-    return `${day}.${month}.${year}.${hours}.${minute}`
-}
+  return `${day}.${month}.${year}.${hours}.${minute}`;
+};
 
-export {
-    formatDateToBR
-}
+export { formatDateToBR };
