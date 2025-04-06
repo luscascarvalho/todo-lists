3d890dd7ee2d66c2c9e81b19a75501f99a0da5f2
Lucas Carvalho <lucascarvalho.mug@gmail.com>
2025-04-06 15:00:32 -0300

    fix: corrigir estrutura de login

--- my-app/src/pages/user/index.tsx
+++ my-app/src/pages/user/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-
-import { Text, View } from "react-native";
+import { style } from "./style";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View, Alert, TouchableOpacity } from "react-native";
+import { useNavigation, NavigationProp } from "@react-navigation/native";
+import Login from "../login";
 
 export default function User() {
+  const navigation = useNavigation<NavigationProp<any>>();
+
+  const handleLogout = () => {
+    Alert.alert("Logout", "Você saiu da conta.");
+    return navigation.reset({ routes: [{ name: "Login" }] });
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Olá user e ai VEI QUE SACO</Text>
+    <View>
+      <Text style={style.name}>Usuário.</Text>
+      <TouchableOpacity style={style.logoutButton} onPress={handleLogout}>
+        <Ionicons name="exit" style={{ color: "gray" }} size={40} />
+      </TouchableOpacity>
     </View>
   );
 }
